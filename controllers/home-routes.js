dc65dd32e91a678602d9f4ca0fbb105c0463ac0a
William Stock <grim118@hotmail.com>
2022-05-16 11:37:36 -0500

    updated main handlebars to show charts on budget page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,23 @@
+const { Script } = require('vm');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('homepage', {
         style: 'style.css'
     })
-})
+});
+
+router.get('/login', (req, res) => {
+    res.render('login', {
+        style: 'loginStyle.css'
+    })
+});
 
 router.get('/budget', (req, res) => {
     res.render('budget', {
         style: 'budgetStyle.css'
     })
-})
+});
 
 module.exports = router;
