5d4bc1f73c085cc758e2865cbc2939269a844141
William Stock <grim118@hotmail.com>
2022-05-17 19:06:16 -0500

    minor handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
-const { Script } = require('vm');
-
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Income } = require('../models');
 
 router.get('/', (req, res) => {
     res.render('homepage', {
@@ -14,10 +14,24 @@ router.get('/login', (req, res) => {
     })
 });
 
+// Get all income
 router.get('/budget', (req, res) => {
-    res.render('budget', {
-        style: 'budgetStyle.css'
+    Income.findAll({
+
     })
-});
+      .then(dbPostData => {
+        // pass a single post object into the homepage template
+        console.log(dbPostData[0])
+        const income = dbPostData
+        res.render('budget', { 
+          income,
+          style: 'budgetStyle.css'
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
