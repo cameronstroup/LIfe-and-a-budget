07d8099b672eba5a7b6d0749a8b2d99aac4773c3
William Stock <grim118@hotmail.com>
2022-05-17 20:24:31 -0500

    added stylesheets

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
 
 router.get('/login', (req, res) => {
     res.render('login', {
-        style: 'loginStyle.css'
+        style: 'login.css'
     })
 });
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const sequelize = require("./config/connection");
 const routes = require("./controllers");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
+const path = require('path');
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -14,6 +15,7 @@ app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 const sess = {
